cc9ed78eb82b45a1f0385b33899e1600ce7f773f
Nicolly Sant'Anna <nicollyengenheira@gmail.com>
2022-12-26 14:35:31 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
-// Variáveis
 
+import { Modal } from "./modal.js"
+
+// Variáveis
 const form = document.querySelector("form")
 const peso = document.querySelector("#peso")
 const altura = document.querySelector("#altura")
 
 
-// ESTRUTURA DE DADOS DO MODAL
-const Modal = {
 
-    wrapper: document.querySelector(".modal-wrapper"),
-    message: document.querySelector(".modal .title span"),
-    buttonClose: document.querySelector(".modal button.close"),
-
-    open(){
-        Modal.wrapper.classList.add('open')
-    },
-    close() {
-        Modal.wrapper.classList.remove('open')
-    }
-
-}
 
 
 form.onsubmit = function (e) {
@@ -35,9 +23,7 @@ form.onsubmit = function (e) {
 
 }
 
-Modal.buttonClose.onclick = () => {
-   Modal.close()
-}
+
 
 function imc (pesoUsuario, alturaUsuario) {
     return (pesoUsuario / ((alturaUsuario / 100) **2 )).toFixed(2)
